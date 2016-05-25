be17c456a6298a725c3079681bca42514697e50e
ngarrett78 <nate@wickedwebdesign.net>
2016-05-25 12:36:48 -0700

    added additional text for testing

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ module.exports = function(express) {
     var router = express.Router();
     
     router.get('/v1', function(req, res) {
-        res.json({hello: "world"});
+        res.json({hello: "world v1 and done"});
     });
     
     router.get('/status', function(req, res){
